1ea31eeb7e884d07ffe1e5b615fbfdb585818969
codexuz <142141576+codexuz@users.noreply.github.com>
2025-09-14 08:36:09 +0500

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AdminModule } from '@adminjs/nestjs';
 import { ConfigModule } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
 
